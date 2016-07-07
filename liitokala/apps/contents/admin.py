0e3be1ed585f9a2ad58c0bb6f1adfdd5ed90f38a
Mathieu Richardoz <mathieu.richardoz@polyconseil.fr>
2016-07-07 17:16:11 +0200

    Improve admin

--- liitokala/apps/contents/admin.py
+++ liitokala/apps/contents/admin.py
@@ -8,4 +8,24 @@ from . import models
 class ContentAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
+    fieldsets = (
+        (None, {
+            'fields': (
+                'title',
+                'slug',
+                'text',
+                'notes',
+                'image',
+            )
+        }),
+        ('Search-related data', {
+            'classes': ('collapse',),
+            'fields': (
+                '_needs_reindexing',
+                '_last_reindexed',
+                '_search_document',
+            ),
+        }),
+    )
+
 admin.site.register(models.Content, ContentAdmin)
